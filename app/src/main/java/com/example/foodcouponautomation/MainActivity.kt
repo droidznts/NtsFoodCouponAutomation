dd6b6882c387e37ba8a986aae7027f62818ca65d
Hegde, Ganesh <ganesh.hegde@news.co.uk>
2019-12-13 22:11:06 +0530

    firebase added user mobile

--- app/src/main/java/com/example/foodcouponautomation/MainActivity.kt
+++ app/src/main/java/com/example/foodcouponautomation/MainActivity.kt
@@ -3,17 +3,67 @@ package com.example.foodcouponautomation
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import kotlinx.android.synthetic.main.activity_main.*
+import com.google.firebase.firestore.FirebaseFirestore
+
+
 
 class MainActivity : AppCompatActivity() {
+
+    var TAG = MainActivity::class.java.canonicalName
+    val db = FirebaseFirestore.getInstance()
+    var isValidMobileNumber = false
+    var list:List<User>  = ArrayList<User>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+
+        db.collection("users")
+            .get()
+            .addOnCompleteListener {
+                    list ->
+                if(list.isSuccessful){
+
+                    var documents = list.result?.documents
+
+                    Log.i(TAG,"vdshcvh");
+                    Log.i(TAG, list.result?.documents?.size.toString());
+                    Log.i(TAG, list.result?.documents?.get(0)?.id);
+                    Log.i(TAG, list.result?.documents?.get(0)?.get("mob")?.toString());
+
+                    isValidMobileNumber = list.result?.documents?.get(0)?.get("mob")?.toString().equals(etMobileNumber.text.toString())
+
+                }else{
+                    Log.i(TAG,"cancel");
+                }
+
+            }
+
+
         submitButton.setOnClickListener {
-            startActivity(Intent(this,CouponsActivity::class.java))
+
+            if(isUserAuthenticated()){
+                startActivity(Intent(this,CouponsActivity::class.java))
+            }
+
         }
 
+
+
+
+
+    }
+
+    private fun isUserAuthenticated(): Boolean {
+
+
+        Log.i(TAG, isValidMobileNumber.toString());
+        Log.i(TAG, etMobileNumber.text.toString());
+
+
+        return isValidMobileNumber
     }
 }
---
+++ app/src/main/java/com/example/foodcouponautomation/User.kt
@@ -0,0 +1,16 @@
+package com.example.foodcouponautomation
+
+data class User(var actInd:String,var bfCoupon:Int,var buUnit:String,var dinnerCoupon:String) {
+
+//    var actInd:String = "";
+//    var bfCoupon:Int = 0
+//    var buUnit:String = ""
+//    var dinnerCoupon:String = ""
+//    var emailId:String = ""
+//    var lunchCoupon = ""
+//    var mobile = 0
+//    var name = ""
+//    var role = ""
+//    var uid = ""
+
+}
