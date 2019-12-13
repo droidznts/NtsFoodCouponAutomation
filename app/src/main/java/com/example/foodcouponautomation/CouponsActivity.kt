8d3045137b51bfc645a8d257ef3abc594aaa7113
Hegde, Ganesh <ganesh.hegde@news.co.uk>
2019-12-14 02:40:00 +0530

    Ui changes

--- app/src/main/java/com/example/foodcouponautomation/CouponsActivity.kt
+++ app/src/main/java/com/example/foodcouponautomation/CouponsActivity.kt
@@ -1,17 +1,47 @@
 package com.example.foodcouponautomation
 
 import `in`.myinnos.androidscratchcard.ScratchCard
+import android.content.DialogInterface
 import android.os.Bundle
+import android.util.Log
+import android.view.View
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.tasks.Task
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.QuerySnapshot
 import kotlinx.android.synthetic.main.activity_coupons.*
 
+
 class CouponsActivity: AppCompatActivity() {
+
+
+    var alert: AlertDialog.Builder? = null
+    val dialog: AlertDialog? = null
+    var TAG = CouponsActivity::class.java.canonicalName
+
+    val db = FirebaseFirestore.getInstance()
+
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        alert = AlertDialog.Builder(this@CouponsActivity)
+
+        val inflater = layoutInflater
+
+        val alertLayout = inflater.inflate(R.layout.layout_custom_dialog, null)
+
+
+//        var letsEatButton = alertLayout.findViewById<TextView>(R.id.btnLetsEat)
+
+        alert!!.setTitle("Enter your Employee Id")
+        // this is set the view from XML inside AlertDialog
+        alert!!.setView(alertLayout)
+        // disallow cancel of AlertDialog on click of back button and outside touch
+        alert!!.setCancelable(false)
 
         setContentView(R.layout.activity_coupons)
 
@@ -20,9 +50,75 @@ class CouponsActivity: AppCompatActivity() {
 
                 if (visiblePercent > 0.3){
                     Toast.makeText(this@CouponsActivity,"Revealed",Toast.LENGTH_SHORT).show()
+
+                    scratch.visibility = View.GONE
                 }
             }
 
         })
+
+
+        alert!!.setPositiveButton("Lets eat", object : DialogInterface.OnClickListener {
+            override fun onClick(dialog: DialogInterface?, which: Int) {
+
+                if (dialog != null) {
+                    dialog.dismiss()
+
+                    redeemButton.visibility = View.GONE
+
+
+                    db.collection("meal_consumption_details")
+                        .get()
+                        .addOnCompleteListener {
+                                snapShot ->
+                            if(snapShot.isSuccessful){
+//                                var documentsList =
+
+
+                                updateUserData(snapShot)
+
+                        }
+
+                }
+            }
+            }
+        })
+
+
+        redeemButton.setOnClickListener {
+
+            val dialog = alert!!.create()
+            dialog.show()
+        }
+
+       /* letsEatButton.setOnClickListener {
+            if (dialog != null) {
+                dialog.dismiss()
+                Toast.makeText(this@CouponsActivity,"Reveal",Toast.LENGTH_SHORT).show()
+            }
+        }*/
+
+
+    }
+
+    private fun updateUserData(snapShot: Task<QuerySnapshot>) {
+
+
+        var documents = snapShot.result?.documents
+
+        if (documents != null) {
+            Log.i(TAG, documents.get(0).get("emp_id").toString());
+
+
+
+            db.collection("users")
+                .get()
+                .addOnCompleteListener {
+
+                }
+
+        }
+
+
     }
 }
--- app/src/main/java/com/example/foodcouponautomation/ProfileActivity.java
+++ app/src/main/java/com/example/foodcouponautomation/ProfileActivity.java
@@ -4,24 +4,38 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import in.myinnos.androidscratchcard.ScratchCard;
 
 import static com.google.android.gms.common.internal.safeparcel.SafeParcelable.NULL;
 
 public class ProfileActivity extends AppCompatActivity {
 
+    private static final String TAG = ProfileActivity.class.getCanonicalName();
     String phoneNumber;
     TextView mobileNumber;
+    TextView redeemButton;
+    ScratchCard scratch;
+    FirebaseFirestore db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+        db = FirebaseFirestore.getInstance();
 
         // get saved phone number
         SharedPreferences prefs = getApplicationContext().getSharedPreferences("USER_PREF",
@@ -30,6 +44,8 @@ public class ProfileActivity extends AppCompatActivity {
 
         mobileNumber = findViewById(R.id.mobileNumber);
         mobileNumber.setText(phoneNumber);
+        redeemButton = findViewById(R.id.redeemButton);
+        scratch = findViewById(R.id.scratch);
 
         findViewById(R.id.buttonLogout).setOnClickListener(new View.OnClickListener() {
             @Override
@@ -41,5 +57,37 @@ public class ProfileActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        scratch.setOnScratchListener(new ScratchCard.OnScratchListener() {
+            @Override
+            public void onScratch(ScratchCard scratchCard, float visiblePercent) {
+                if(visiblePercent > 0.3){
+                    scratch.setVisibility(View.INVISIBLE);
+                }
+            }
+        });
+
+        redeemButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                updateUserDatabase();
+
+            }
+        });
+    }
+
+    private void updateUserDatabase() {
+        db.collection("users")
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+
+                        if(task.isSuccessful()){
+                            Log.i(TAG, String.valueOf(task.getResult().getDocuments().size()));
+                        }
+
+                    }
+                });
     }
 }
